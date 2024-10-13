81cb2cabde87aa40b1f8ebf51883259b0ff58e59
Mayank Issrani <mayankissrani@gmail.com>
2024-10-13 16:48:36 +0530

    .

--- 0075-sort-colors/0075-sort-colors_app2.cpp
+++
@@ -1,9 +0,0 @@
-// this solution has a time complexity of O(nlogn)
-
-class Solution {
-public:
-    void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        
-    }
-};
