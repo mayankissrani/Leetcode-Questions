1b04824b321f9d5629099de5eecf92624d0731eb
Mayank Issrani <mayankissrani@gmail.com>
2024-10-13 16:46:42 +0530

    changed the names

---
+++ 0075-sort-colors/BruteForce.cpp
@@ -0,0 +1,9 @@
+// this solution has a time complexity of O(nlogn)
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        
+    }
+};
